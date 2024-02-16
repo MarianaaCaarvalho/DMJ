77fad36186ad2166e9707bc3b206e2ac284dbb0f
jinnFreddy <jorge.f.curto@gmail.com>
2024-02-16 19:08:17 +0000

    Revert "reupload"
    This reverts commit e319df004cbf9409e12a47b116df7f91f983287e.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,11 +9,7 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent _enemy;
     private GameObject _player;
-    public Animator animator;
-    private int lives = 3;
-    private bool livesChanged = false;
-    private float _timer;
-
+    
     // Start is called before the first frame update
     void Awake()
     {
@@ -27,35 +23,13 @@ public class EnemyAI : MonoBehaviour
         NavMeshPath _path = new NavMeshPath();
         _enemy.CalculatePath(_player.transform.position, _path);
         _enemy.SetPath(_path);
-
-        if (livesChanged)
-        {
-            animator.enabled = false;
-            _enemy.speed = 0f;
-            _timer += Time.deltaTime;
-            if (_timer > 2f)
-            {
-                _enemy.speed = 1f;
-                _timer = 0;
-                livesChanged = false;
-                animator.enabled = true;
-            }
-        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            if (lives == 0)
-            {
-                SceneManager.LoadScene("DeadScene");
-            }
-            else
-            {
-                lives -= 1;
-                livesChanged = true;
-            }
+            SceneManager.LoadScene(1);
         }
     }
 }
