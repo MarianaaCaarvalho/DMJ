4ecbabeb92497a11dd215bf5ecf2dc9faa7fdfbe
jinnFreddy <jorge.f.curto@gmail.com>
2024-02-16 22:44:30 +0000

    fix

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,7 +9,11 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent _enemy;
     private GameObject _player;
-    
+    public Animator animator;
+    private int lives = 3;
+    private bool livesChanged = false;
+    private float _timer;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -23,13 +27,35 @@ public class EnemyAI : MonoBehaviour
         NavMeshPath _path = new NavMeshPath();
         _enemy.CalculatePath(_player.transform.position, _path);
         _enemy.SetPath(_path);
+
+        if (livesChanged)
+        {
+            animator.enabled = false;
+            _enemy.speed = 0f;
+            _timer += Time.deltaTime;
+            if (_timer > 2f)
+            {
+                _enemy.speed = 1f;
+                _timer = 0;
+                livesChanged = false;
+                animator.enabled = true;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(1);
+            if (lives == 0)
+            {
+                SceneManager.LoadScene("DeadScene");
+            }
+            else
+            {
+                lives -= 1;
+                livesChanged = true;
+            }
         }
     }
 }
